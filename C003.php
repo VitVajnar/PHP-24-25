a35266aab6b87c149e3b7ada1454f178158b82a9
vajny <144332165+VitVajnar@users.noreply.github.com>
2024-10-31 16:17:26 +0100

    Update C003.php

--- C003.php
+++ C003.php
@@ -2,13 +2,13 @@
 
 $pole = ["citron", "banan", "citron", "jablko"];
 
-$nonduplicat = array_unique($pole);
+$pole1 = array_unique($pole);
 
 $pole2 = [5, 10, 15];
 
-$pole2_soucet = array_sum($pole2);
+$pole23 = array_sum($pole2);
 
-print_r($pole2_soucet);
+print_r($pole23);
 
 
 
