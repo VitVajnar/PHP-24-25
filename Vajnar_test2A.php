b1b01d8438d5ed3283230c7aeac40bd678be0be6
vajny <144332165+VitVajnar@users.noreply.github.com>
2024-11-01 10:26:46 +0100

    Add files via upload

---
+++ Vajnar_test2A.php
@@ -0,0 +1,15 @@
+<?php
+$pole = array (1,2,3,4);
+echo array_sum($pole);
+echo '<br>';
+print_r($pole);
+$pole[9];
+echo'<br>';
+implode(".", $pole);
+echo'<br>';
+sort($pole);
+print_r($pole);
+echo'<br>';
+rsort($pole);
+print_r($pole);
+?>
